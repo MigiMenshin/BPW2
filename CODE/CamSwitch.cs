28937cb476f1f20c441d49653e72f7035747e021
DESKTOP-SHIN\Migue <miguel_mensinga@hotmail.com>
2024-06-30 18:54:02 +0200

    Assets added back to project

---
+++ CODE/CamSwitch.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CamSwitch : MonoBehaviour
+{
+    public Transform Player;
+    public CinemachineVirtualCamera activeCam;
+    public CinemachineVirtualCamera followCam;
+    private static List<CinemachineVirtualCamera> allCams = new List<CinemachineVirtualCamera>();
+
+    private void Awake()
+    {
+        // Add the camera to the list of all cameras
+        if (!allCams.Contains(activeCam))
+        {
+            allCams.Add(activeCam);
+        }
+    }
+
+    private void Start()
+    {
+        // Set initial priorities
+        followCam.Priority = 10;
+        activeCam.Priority = 0;
+
+        // Check if the player is within the camera's trigger zone at the start
+        Collider[] colliders = Physics.OverlapSphere(Player.position, 0.1f);
+        foreach (var collider in colliders)
+        {
+            if (collider == GetComponent<Collider>())
+            {
+                SetActiveCamera();
+                break;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player entered trigger zone: " + gameObject.name);
+            SetActiveCamera();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player exited trigger zone: " + gameObject.name);
+            ResetFollowCamera();
+        }
+    }
+
+    private void SetActiveCamera()
+    {
+        // Set the priority of all cameras to 0
+        foreach (var cam in allCams)
+        {
+            cam.Priority = 0;
+        }
+
+        // Set the priority of the active camera to 1
+        activeCam.Priority = 1;
+        followCam.Priority = 0;  // Set follow camera priority to 0
+        Debug.Log("Active camera set to: " + activeCam.name);
+    }
+
+    private void ResetFollowCamera()
+    {
+        // Reset the follow camera priority
+        followCam.Priority = 10;
+        activeCam.Priority = 0;
+        Debug.Log("Follow camera reset.");
+    }
+}
